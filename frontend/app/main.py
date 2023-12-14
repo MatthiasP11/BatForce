452c4d8e203371c58b0666635a6b3484212af74e
Ho se Lee <hoselee@MacBook-Pro-di-Ho-40.local>
2023-12-14 15:30:16 +0100

    add wtforms

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -8,7 +8,6 @@ from flask import Flask, render_template
 import requests  # Import the requests library to make HTTP requests
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with a secure secret key
