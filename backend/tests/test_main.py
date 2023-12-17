179fe018d50cc11ea58d4c0cc17539d3bcaca64c
hoselee07 <ho.lee@student.h-farm.com>
2023-12-17 19:04:32 +0100

    Your commit message

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -2,16 +2,8 @@ import os
 import sys
 from fastapi.testclient import TestClient
 
-# Add the project root to the sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-# Now you can do the relative import
-from app.main import app
-
-
-"""
-Execute this test by running on the terminal (from the app/) the command:
-pytest --cov=app --cov-report=html tests/
- """
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(_file_), '..')))
+from app.main import app  
 
 client = TestClient(app)
 
