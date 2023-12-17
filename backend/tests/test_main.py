44b105d1a58079fdb8ccb2a460748c58aaa9d917
hoselee07 <ho.lee@student.h-farm.com>
2023-12-17 20:06:34 +0100

    Struttura prof progetto

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -2,42 +2,31 @@ import os
 import sys
 from fastapi.testclient import TestClient
 
-# Add the project root to the sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-# Now you can do the relative import
-from app.main import app
-
-
-"""
-Execute this test by running on the terminal (from the app/) the command:
-pytest --cov=app --cov-report=html tests/
- """
+from app.main import app  
 
 client = TestClient(app)
 
-
 def test_read_main():
+    """
+    Test the main route of the FastAPI application.
+
+    This test ensures that the main route ('/') is accessible and
+    returns the expected response.
+    """
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
 
+def test_api_endpoint_valid_input():
+    """
+    Test the API endpoint with valid input.
 
-def test_success_read_item():
-    response = client.get("/query/Albert Einstein")
-    assert response.status_code == 200
-    assert response.json() == {"person_name": 'Albert Einstein', 
-                               "birthday": '03/14/1879'}
-
-
-""" def test_fail_read_item():
-    response = client.get("/query/Pippo")
-    assert response.status_code == 200
-    assert response.json() == {"error": "Person not found"} """
-
-
-# The following will generate an error in pycheck
-""" def test_success_read_item_module():
-    response = client.get("/module/search/Albert Einstein")
+    This test checks the response of the API when a valid 'comune' and 'anno'
+    are provided in the request. For this test, 'Affi' and '1997' are used.
+    It validates both the status code and the response content.
+    """
+    response = client.get("/rifiuto/Affi/1997")
     assert response.status_code == 200
     assert response.json() == {"Rifiuto totale (in Kg)": "1.155.208"}
 
