386869f4a3f270087e528312c11aa3b8bf3fb39b
burix22 <150247448+burix22@users.noreply.github.com>
2023-12-17 12:51:37 +0100

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,68 +1,47 @@
-"""
-Backend module for the FastAPI application.
-
-This module defines a FastAPI application that serves
-as the backend for the project.
-"""
-
-
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from datetime import datetime
 import pandas as pd
+from fastapi import FastAPI, HTTPException
+
+# The following code is responsible for loading data from a CSV file into a pandas DataFrame.
+# It then sets up a FastAPI application with endpoints to access specific data based on query parameters.
+
+# Load the CSV file
+csv_file_path = '/app/app/filedati.csv'  # Path to the CSV file containing the data
+csv_data = pd.read_csv(csv_file_path, encoding='ISO-8859-1', delimiter=';')  # Loading the CSV data
+csv_data['Anno'] = csv_data['Anno'].astype(int)  # Converting the 'Anno' column to integers for year representation
+csv_data['Comune'] = csv_data['Comune'].astype(str)  # Ensuring the 'Comune' column is of string type
+csv_data['Rifiuto totale (in Kg)'] = csv_data['Rifiuto totale (in Kg)'].astype(str)  # Converting 'Rifiuto totale' to string
+
+app = FastAPI()  # Initializing the FastAPI app
+
+@app.get("/rifiuto/{comune}/{anno}")
+def get_rifiuto_totale(comune: str, anno: int):
+    """
+    Endpoint to get the total waste (Rifiuto totale in Kg) for a specified 'Comune' (city/town) and 'Anno' (year).
+    The function queries the loaded CSV data to find the matching record.
+
+    Args:
+    comune (str): The name of the city or town.
+    anno (int): The year for which the data is required.
+
+    Returns:
+    str: The total waste in kilograms for the specified 'Comune' and 'Anno' or an HTTP 404 error if data is not found.
+    """
+    # Filtering data for the specified 'Comune' and 'Anno'
+    data = csv_data[(csv_data['Comune'] == comune) & (csv_data['Anno'] == anno)]
     
+    # Handling the case where no data is found
+    if data.empty:
+        raise HTTPException(status_code=404, detail="Data not found")
+    
+    return data['Rifiuto totale (in Kg)'].iloc[0]  # Returning the total waste for the specified parameters
 
-
-
-app = FastAPI()
-
-# Load the new CSV data
-df = pd.read_csv('/app/app/filedati.csv', encoding= "latin-1", sep=";")
-@app.get("/total_waste")
-def read_total_waste(comune: str, anno: int):
-    """
-    Endpoint to get the total waste for a given "Comune" and "Anno".
-    """
-    data = df[(df['Comune'] == comune) & (df['Anno'] == anno)]
-    if not data.empty:
-        return {"Rifiuto totale (in Kg)": data["Rifiuto totale (in Kg)"].sum()}
-    else:
-        return JSONResponse(status_code=404, content={"message": "Data not found for the provided Comune and Anno."})
-
-@app.get("/total_waste_sum")
-def read_total_waste_sum(comune: str):
-    """
-    Endpoint to get the sum of all "Rifiuto totale (in Kg)" for a given "Comune" across all years.
-    """
-    data = df[df['Comune'] == comune]
-    if not data.empty:
-        return {"Rifiuto totale (in Kg) sum": data["Rifiuto totale (in Kg)"].sum()}
-    else:
-        return JSONResponse(status_code=404, content={"message": "Data not found for the provided Comune."})
-
-
-
-@app.get('/')
+@app.get("/")
 def read_root():
     """
-    Root endpoint for the backend.
+    Root endpoint of the API.
 
     Returns:
-        dict: A simple greeting.
+    dict: A simple greeting message.
     """
-    return {"Hello": "World"}
+    return {"Hello": "World"}  # A basic response for the root endpoint
 
-@app.get('/get-date')
-def get_date():
-    """
-    Endpoint to get the current date.
-
-    Returns:
-        dict: Current date in ISO format.
-    """
-    current_date = datetime.now().isoformat()
-    return JSONResponse(content={"date": current_date})
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
